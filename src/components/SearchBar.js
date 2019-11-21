bf3fade129e75cfa1e9067e4c9646d2d6000a879
github username <github email address>
2019-11-21 17:52:48 -0500

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+// import { makeStyles } from '@material-ui/core/styles'
+// import { Paper, InputBase, Divider, IconButton } from '@material-ui/core'
+// import { Menu, Search } from '@material-ui/icons'
+
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//         padding: '2px 4px',
+//         display: 'flex',
+//         alignItems: 'center',
+//         width: 400,
+//     },
+//       input: {
+//         marginLeft: theme.spacing(1),
+//         flex: 1,
+//     },
+//       iconButton: {
+//         padding: 10,
+//     },
+//       divider: {
+//         height: 28,
+//         margin: 4,
+//     },
+// }))
+
+const SearchBar = () => {
+    return (
+        <h1>hi</h1>
+    )
+}
+export default SearchBar
