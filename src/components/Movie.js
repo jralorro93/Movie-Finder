88309ac541de5937fc493adb7153dc84274c4938
github username <github email address>
2019-11-27 15:46:03 -0500

    working on view page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,24 @@
 import React from 'react'
 
+import viewMoviePage from '../utils/movies/viewMoviePage'
+
 const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
 const Movie = ({movie}) => {
-    // console.log('this is props from movie', props)
-    const moviePoster = movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster
+    console.log('this is props from movie', movie)
+    
     return (
         <div>
-            <h2>{movie.Title}</h2>
+            <h2>{movie.title}</h2>
             <div>
                 <img 
                     width='200'
-                    alt={movie.Title}
-                    src={moviePoster}
+                    alt={movie.title}
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 />
             </div>
-            <p>{movie.year}</p>
+            <p>{movie.release_date}</p>
+            <button onClick={ () => viewMoviePage(movie.id)}>View Page</button>
         </div>
     )
 }
---
+++ src/components/ViewPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ViewPage = (props) => {
+    return (
+        <div>Hi</div>
+    )
+}
+export default ViewPage
