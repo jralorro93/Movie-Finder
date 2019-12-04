3d759a21b1480e8b00758e905b3d4cd313e3ed70
github username <github email address>
2019-12-03 22:03:05 -0500

    working on view page

--- src/components/ViewPage.js
+++ src/components/ViewPage.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState} from 'react'
 
-const ViewPage = (props) => {
+import viewMoviePage from '../utils/movies/viewMoviePage'
+
+import { Modal } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = (theme => ({
+
+}))
+
+const ViewPage = ({movie}) => {
+    const classes = useStyles()
+    const [ open, setOpen ] = useState(false)
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    
+    const handleClose = () => {
+        setOpen(false)
+    }
+
     return (
-        <div>Hi</div>
+        <div>
+            <button onClick={ () => {viewMoviePage(movie.id); handleOpen()}}>View Page</button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <div>
+                    <h2>Hi</h2>
+                    <h3>No</h3>
+                </div>
+            </Modal>
+        </div>
     )
 }
 export default ViewPage
