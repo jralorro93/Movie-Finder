f831cf41043e0615ab9e47c8135ab69bbd82f3ee
github username <github email address>
2019-12-04 18:04:16 -0500

    working on view page

--- src/components/ViewPage.js
+++ src/components/ViewPage.js
@@ -1,4 +1,5 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 
 import viewMoviePage from '../utils/movies/viewMoviePage'
 
@@ -12,6 +13,7 @@ const useStyles = (theme => ({
 const ViewPage = ({movie}) => {
     const classes = useStyles()
     const [ open, setOpen ] = useState(false)
+    const [ movieDetails, setMovieDetails ] = useState({})
 
     const handleOpen = () => {
         setOpen(true)
@@ -20,10 +22,19 @@ const ViewPage = ({movie}) => {
     const handleClose = () => {
         setOpen(false)
     }
-
+    
+    useEffect(() => {
+        const fetchMovieInfo = async () => {
+            const url = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=a64ac52df84876407a110b1db357ebe3`
+            const res = await axios.get(url)
+            setMovieDetails(res.data)
+        } 
+        fetchMovieInfo()
+    }, [])
+    
     return (
         <div>
-            <button onClick={ () => {viewMoviePage(movie.id); handleOpen()}}>View Page</button>
+            <button onClick={ () => handleOpen()}>View Page</button>
             <Modal
                 open={open}
                 onClose={handleClose}
