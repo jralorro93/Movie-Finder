4134f54020d06664a8163e2df2dde0ae3db30a2c
github username <github email address>
2019-12-07 15:43:15 -0500

    starting images carosel

---
+++ src/components/ImageCarosel.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    images: {
+        
+    }
+}))
+
+const ImageCarosel = ({images}) => {
+    const classes = useStyles()
+    return (
+        <div>Hi</div>
+    )
+}
+export default ImageCarosel
